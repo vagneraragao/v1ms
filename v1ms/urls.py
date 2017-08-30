3996299ccf9e5f504ad91406f47f7593d2315b07
Vagner Aragao <vmaragao@gmail.com>
2017-08-30 15:39:02 -0300

    Funcional com Form

--- v1ms/urls.py
+++ v1ms/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.shortcuts import redirect
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls')),
     url(r'', include('blog.urls')),
 ]
