10d9a6e5e91346a34be256eee482e8615bf5bf49
Vagner Aragao <vmaragao@gmail.com>
2023-04-09 21:58:58 +0100

    Adicionados

--- v1ms/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.shortcuts import redirect
-
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('blog.urls')),
-    url(r'', include('blog.urls')),
-]
